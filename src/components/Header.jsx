ea223c18e881261fbb32d2d8e4efab56c69c89db
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-07-22 00:17:49 -0500

    Add functional theme change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,29 @@ import { useEffect, useState } from 'react';
 import '../assets/styles/components/header.scss'
 
 const Header = () => {
+    const darkTheme = 'dark-theme'
+    const [iconName, setIconName] = useState('')
+
+    const setIconTheme = () => {
+        let isDark = document.body.classList.contains(darkTheme)
+        setIconName(isDark ? 'icon-moon-fill' : 'icon-moon-stroke')
+    }
+
+    const handleClick = () => {
+        document.body.classList.toggle(darkTheme)
+        setIconTheme()
+    }
+
+    useEffect(() => {
+        setIconTheme()
+    }, []);
+
     return (
         <header>
             <nav>
                 <h1>Where in the world?</h1>
-                <button>
-                    <span className="icon icon-moon-stroke">
+                <button onClick={handleClick} >
+                    <span className={`icon ${iconName}`}>
                     </span> Dark Mode
                 </button>
             </nav>
