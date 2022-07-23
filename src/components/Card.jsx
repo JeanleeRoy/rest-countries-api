115c008399c03e3c2d5f38051722693a0c696b62
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-07-23 01:30:21 -0500

    Update country card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,23 @@
 import '../assets/styles/components/card.scss'
 
-const Card = ({country}) => {
-    return ( 
+const Card = ({ country }) => {
+    return (
         <div className="country-card">
             <div className="country-image">
-                <img src="https://flagcdn.com/w320/de.png" alt="country" />
+                <img src={country.flags.png} alt="country" />
             </div>
             <div className="country-description">
-                <h2>Germany</h2>
+                <h2>{country.name.common}</h2>
                 <div className="country-detail">
-                    <p><b className='strong'>Population:</b> 81,770,900</p>
-                    <p><b className='strong'>Region:</b> Europe</p>
-                    <p><b className='strong'>Capital:</b> Berlin</p>
+                    <p><b className='strong'>
+                        Population:</b> {country.population.toLocaleString('en-US')}
+                    </p>
+                    <p><b className='strong'>Region:</b> {country.region}</p>
+                    <p><b className='strong'>Capital:</b> {country.capital}</p>
                 </div>
             </div>
         </div>
-     )
+    )
 }
- 
+
 export default Card;
