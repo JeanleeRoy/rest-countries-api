84a53c35995170c92d9939dd036258ed8bf6b3ea
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-07-24 20:59:23 -0500

    Update country view

--- src/views/Country.jsx
+++ src/views/Country.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import handleUrl from "../utils/countriesApi"
+
 const Country = () => {
-    return ( 
+    const [country, setCountry] = useState(null)
+    const { name } = useParams()
+
+    const fetchCountry = () => {
+        fetch(handleUrl(name))
+        .then(res => res.json())
+        .then(data => {
+            setCountry(data[0])
+        })
+    }
+
+    useEffect(() => {
+        fetchCountry()
+    }, [])
+
+    return (
         <>
-            <p>Country Deatails</p>
+            <p>{country?.name.common} Deatails</p>
         </>
-     );
+    );
 }
- 
+
 export default Country;
